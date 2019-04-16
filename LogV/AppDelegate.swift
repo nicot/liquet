a8fdbddbc43b76b4746d2614ec1b3e5a028dc8a5
Dominic Tonozzi <dtonozzi@twitter.com>
2019-04-16 10:54:15 -0700

    Initial Commit

---
+++ LogV/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LogV
+//
+//  Created by Dominic Tonozzi on 4/16/19.
+//  Copyright © 2019 Nico T. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LogV/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LogV
+//
+//  Created by Dominic Tonozzi on 4/16/19.
+//  Copyright © 2019 Nico T. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
